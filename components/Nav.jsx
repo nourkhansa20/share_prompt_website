d5f36fef31dd773c364ee25a62636d19147b2d47
Nour Khansa <118853035+nourkhansa20@users.noreply.github.com>
2024-04-25 11:22:14 +0300

    egnore .env

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -38,7 +38,7 @@ const Nav = () => {
         <nav className="flex-between w-full mb-16 pt-3">
             <Link href="/" className="flex gap-2 flex-center">
                 <Image src={'/assets/images/logo.svg'} alt="Promptopia Logo" width={30} height={30} className="object-contain" />
-                <p className="logo_text">Promptopia</p>
+                <p className="logo_text">Emporium</p>
             </Link>
 
             {/* Desktop Navigation  */}
